87deea3c47e2a00e820fbf07c4fa61271c385379
dxy <daixianyou@zjut.edu.cn>
2024-07-11 19:30:14 +0800

    feat: 添加点云可视化测试页面

--- model/falsk_demo.py
+++ model/falsk_demo.py
@@ -6,20 +6,10 @@ app = Flask(__name__)
 CORS(app)
 
 
-@app.route('/model/test/<f_type>/<f_name>', methods=['GET'])
-def download_file(f_type, f_name):
-    try:
-        # 读取文件并进行 base64 编码
-        f_path = './src/' + f_type + '/' + f_name
-        with open(f_path, 'rb') as file:
-            file_data = file.read()
-            encoded_data = base64.b64encode(file_data).decode('utf-8')
-
-        # 返回编码后的数据
-        return jsonify({'filename': f_name, 'file_data': encoded_data})
-
-    except FileNotFoundError:
-        return jsonify({'error': 'File not found'}), 404
+@app.route('/bin/<f_name>', methods=['GET'])
+def get_bin(f_name):
+    with open('./src/pcd/' + f_name, 'rb') as f:
+        return jsonify({'data': base64.b64encode(f.read()).decode('utf-8')})
 
 
 if __name__ == '__main__':
